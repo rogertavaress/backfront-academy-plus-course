3aca36f79ee8bc8594fbed09ec04f55ec94ff346
Rogério Tavares <email@rogeriotavares.dev>
2024-03-02 11:10:05 -0300

    module-02: create iboutlet

--- module-02/module-02-02/module-02-02/ViewController.swift
+++ module-02/module-02-02/module-02-02/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Elements
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        welcomeLabel.textColor = .white
     }
 
 
