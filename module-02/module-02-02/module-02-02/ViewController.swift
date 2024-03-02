c28507bd4e5027475aaa23a335345d4bb6ab74c1
Rogério Tavares <email@rogeriotavares.dev>
2024-03-02 10:52:18 -0300

    module-02: create uiview and uitextfield

---
+++ module-02/module-02-02/module-02-02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  module-02-02
+//
+//  Created by Rogério Tavares on 02/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
